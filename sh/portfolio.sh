91409bf3c87765bb3e00f936fb2d6a1caf6b71c5
larry <larry@study64>
2015-10-15 19:21:26 +0800

    new chart types

--- sh/portfolio.sh
+++ sh/portfolio.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-ROOT=/home/larry/l1304/production/finopt
+ROOT=/home/larry/l1304/workspace/finopt/src
 export PYTHONPATH=$ROOT
 python $ROOT/finopt/portfolio.py $ROOT/config/app.cfg
--- sh/run_options_data.sh
+++ sh/run_options_data.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-ROOT={replace-path}
+#ROOT={replace-path}
+ROOT=~/l1304/workspace/finopt
 export PYTHONPATH=$ROOT/src
 python $ROOT/src/finopt/options_data.py $ROOT/src/config/app.cfg
